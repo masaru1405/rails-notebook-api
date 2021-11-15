422a9cd41ec143f469d33348716871a3f7fdfd2b
Kaio Masaru Nakazono <kmn1405.cg@gmail.com>
2021-11-15 04:47:52 +0000

    add HATEOAS

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,9 +1,19 @@
 class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate #, :author
 
-  belongs_to :kind
+  belongs_to :kind do
+    link(:kind) {kind_url(object.kind.id)}
+  end
+
   has_many :phones
   has_one :address
+
+  #link(:self) {contact_path(object.id)}
+  #link(:kind) {kind_path(object.kind.id)}
+
+  #ou passando a url (devemos configurar o host e a porta em development e/ou production)
+  link(:self) {contact_url(object.id)}
+  link(:kind) {kind_url(object.kind.id)}
 
   # def author
   #   "Kaio"
